68700a298a28a515ff45b28131da5e78468c84a7
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2024-07-10 12:36:09 +0300

    fixed some styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import '@epam/uui-components/styles.css';
 import '@epam/uui/styles.css';
 import './assets/theme/theme-faraway.scss';
-import './index.module.scss';
+import './index.scss';
 //
 import { StrictMode } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
